658a1bec25fc7b40cf36a27d0e9eff4875626f25
dieskay-coder <66352779+dieskay-coder@users.noreply.github.com>
2021-05-31 15:51:06 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,17 +20,27 @@
       this.el.textContent = num;
     }
 
+    // currentNumと押し込んだ数値が合っているかをチェックする関数
     check() {
       if (this.game.getCurrentNum() === parseInt(this.el.textContent, 10)) {
+        soundB.load();
+        soundB.play();
         this.el.classList.add('pressed');
         this.game.addCurrentNum();
 
+        // 全て押し込むと、時間を止める
         if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
+          window.setTimeout (() => {
+            soundY.load();
+            soundY.play();
+          },1000);
           clearTimeout(this.game.getTimeoutId());
         }
+      }else {
+        soundNg.load();
+        soundNg.play();
       }
     }
-
   }
   
   class Board {
@@ -56,12 +66,12 @@
         nums.push(i);
       }
 
+      // numsからspliceした要素の0番目を順に抜き出している
       this.panels.forEach(panel => {
         const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
         panel.activate(num);
       });
     }
-
   }
 
   
@@ -77,10 +87,13 @@
       const btn = document.getElementById('btn');
       btn.addEventListener('click', () => {
         this.start();
+        soundS.load();
+        soundS.play();
       });
       this.setup();
     }
 
+    // 動的にサイズを変えるためのメソッド
     setup() {
       const container =document.getElementById('container');
       const PANEL_WIDTH = 100;
@@ -88,6 +101,7 @@
       container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
     }
 
+    // タイマーが2重に動かないようにするメソッド
     start() {
       if (typeof this.timeoutId !== 'undefined') {
         clearTimeout(this.timeoutId);
@@ -108,9 +122,7 @@
         this.runTimer();
       }, 10);
     }
-      
 
-    
     addCurrentNum() {
       this.currentNum++;
     }
@@ -127,5 +139,12 @@
       return this.level;
     }
   }
-  new Game(3);
+
+  const soundS = new Audio("start.mp3");
+  const soundB = new Audio("button.mp3");
+  const soundY = new Audio("yay.mp3");
+  const soundNg = new Audio("ng.mp3");
+
+  // ここの数値で難易度設定
+  new Game(4);
 }
